4848463ab4db3c90710e9cd8a91b2bd1a82d127e
rafeywaleed <a.rafeywaleeda5@gmail.com>
2025-02-02 23:45:36 +0530

    Notes page working

---
+++ lib/utils/notes_theme_list.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final List<Map<String, dynamic>> themes = [
+  // Default theme
+
+  {
+    'titleFont': 'Poppins',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(33, 29, 30, 1),
+    'backgroundColor': Color.fromRGBO(233, 230, 225, 1),
+  },
+  // Classic Themes
+  {
+    'titleFont': 'Plus Jakarta Sans',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(80, 49, 47, 1),
+    'backgroundColor': Color.fromRGBO(219, 204, 181, 1),
+  },
+  {
+    'titleFont': 'Playfair Display',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(240, 214, 187, 1),
+    'backgroundColor': Color.fromRGBO(53, 53, 53, 1),
+  },
+
+  {
+    'titleFont': 'Poppins',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(204, 49, 61, 1),
+    'backgroundColor': Color.fromRGBO(247, 197, 204, 1),
+  },
+  {
+    'titleFont': 'Oswald',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(38, 25, 17, 1),
+    'backgroundColor': Color.fromRGBO(249, 246, 239, 1),
+  },
+  {
+    'titleFont': 'Lustria',
+    'bodyFont': 'Lato',
+    'textColor': Color.fromRGBO(245, 240, 236, 1),
+    'backgroundColor': Color.fromRGBO(0, 0, 0, 1),
+  },
+
+  {
+    'titleFont': 'Lora',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(235, 235, 235, 1),
+    'backgroundColor': Color.fromRGBO(55, 66, 88, 1),
+  },
+  // Elegant Themes
+  {
+    'titleFont': 'Cinzel',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(219, 204, 181, 1),
+    'backgroundColor': Color.fromRGBO(181, 104, 76, 1),
+  },
+  {
+    'titleFont': 'Sacramento',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(80, 49, 47, 1),
+    'backgroundColor': Color.fromRGBO(219, 204, 181, 1),
+  },
+  {
+    'titleFont': 'Yeseva One',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(219, 169, 136, 1),
+    'backgroundColor': Color.fromRGBO(22, 44, 58, 1),
+  },
+  {
+    'titleFont': 'Cardo',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(219, 172, 111, 1),
+    'backgroundColor': Color.fromRGBO(123, 51, 52, 1),
+  },
+  {
+    'titleFont': 'Lora',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(33, 29, 30, 1),
+    'backgroundColor': Color.fromRGBO(233, 230, 225, 1),
+  },
+  {
+    'titleFont': 'Spectral',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(249, 248, 246, 1),
+    'backgroundColor': Color.fromRGBO(7, 54, 48, 1),
+  },
+
+  {
+    'titleFont': 'Halant',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(209, 202, 194, 1),
+    'backgroundColor': Color.fromRGBO(29, 29, 29, 1),
+  },
+  {
+    'titleFont': 'Abril Fatface',
+    'bodyFont': 'Poppins',
+    'textColor': Color.fromRGBO(37, 31, 84, 1),
+    'backgroundColor': Color.fromRGBO(209, 200, 255, 1),
+  },
+  // Modern Themes
+  {
+    'titleFont': 'Roboto',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(35, 31, 32, 1),
+    'backgroundColor': Color.fromRGBO(173, 142, 121, 1),
+  },
+  {
+    'titleFont': 'Quicksand',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(15, 12, 19, 1),
+    'backgroundColor': Color.fromRGBO(230, 225, 222, 1),
+  },
+  {
+    'titleFont': 'Oswald',
+    'bodyFont': 'Merriweather',
+    'textColor': Color.fromRGBO(249, 249, 249, 1),
+    'backgroundColor': Color.fromRGBO(51, 47, 48, 1),
+  },
+  {
+    'titleFont': 'Ubuntu',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(235, 224, 218, 1),
+    'backgroundColor': Color.fromRGBO(60, 53, 43, 1),
+  },
+  {
+    'titleFont': 'Nunito',
+    'bodyFont': 'Open Sans',
+    'textColor': Color.fromRGBO(236, 232, 231, 1),
+    'backgroundColor': Color.fromRGBO(46, 54, 65, 1),
+  },
+  {
+    'titleFont': 'Oswald',
+    'bodyFont': 'Merriweather',
+    'textColor': Color.fromRGBO(221, 225, 236, 1),
+    'backgroundColor': Color.fromRGBO(46, 54, 65, 1),
+  },
+  {
+    'titleFont': 'Montserrat',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(55, 57, 69, 1),
+    'backgroundColor': Color.fromRGBO(220, 221, 225, 1),
+  },
+  // Creative Themes
+  {
+    'titleFont': 'Arvo',
+    'bodyFont': 'Lato',
+    'textColor': Color.fromRGBO(3, 5, 4, 1),
+    'backgroundColor': Color.fromRGBO(151, 235, 212, 1),
+  },
+
+  {
+    'titleFont': 'Merriweather',
+    'bodyFont': 'Lora',
+    'textColor': Color.fromRGBO(226, 223, 218, 1),
+    'backgroundColor': Color.fromRGBO(38, 38, 38, 1),
+  },
+];
